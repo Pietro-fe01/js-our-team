1db68a020714b201505f1c9c56faea70bed93cd7
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-07 14:02:40 +0100

    Stampo nella console tutte le informazioni relative ad ogni oggetto nell'array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+"use strict";
+
 /* 
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
 */ 
 
 const teamMembers = [
@@ -43,15 +48,22 @@ const teamMembers = [
 
 console.log(teamMembers);
 
+/* MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto */
+for(let i = 0; i < teamMembers.length; i++){
+    console.log(`Oggetto numero ${i + 1}:`);
+    for(let key in teamMembers[i]){
+        console.log(teamMembers[i][key]);
+    }
+};
+
 /*
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
+
 BONUS 2:
 Organizzare i singoli membri in card/schede
 */
